f7ea2ad020352763b7d278b22f67a9314a5dc4bb
KahuanKZ1 <kahuan.koike@escola.pr.gov.br>
2025-04-22 08:49:08 -0300

    script

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
     })
 })
+
+const alternaContraste = document.getElementById('alterna-contraste')
+
+alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste')
+})
